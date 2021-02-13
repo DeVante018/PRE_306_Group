50833aaf7d416c7cad99815b2dbbef316a6643b5
DeVante018 <devantef@buffalo.edu>
2021-02-13 14:53:15 -0500

    Created main function logic to parse command line arguments and added helper functions

--- main.c
+++ main.c
@@ -1,29 +1,64 @@
 #include <stdio.h>
 #include <string.h>
-/** csv [-frh] [-max field] [-min field] [-mean field] [-records field value] file **/
+#include  <stdbool.h>
+/** csv [-frh] {[-max field] [-min field] [-mean field]} [-records field value] file **/
 
 //first field
 void commandF(char*);
 void commandR(char*);
 void commandH(char*);
 
-void maxField(char*);// not sure for parameters
-void minField(char*);
-void meanField(char*);
-void recordsFieldValue(char*);
+void maxField(char*, char*, char*);// not sure for parameters
+void minField(char*, char*, char*);
+void meanField(char*, char*, char*);
+void recordsFieldValue(char*, char*, char*);
+int checkInputError(int, char*);
+void initOptions(char**);
+
+void printError();
 
 int main(int argc, char *argv[]){
-    if(argc  <= 3){
-        printf("invalid command line arguments\n");
-        printf("Use: csv [-frh] [-max field] [-min field] [-mean field] [-records field value] file\n");
+
+    int arguments = checkInputError(argc, argv[1]);
+    if(arguments == -1){
+        printError();
+        return 0;
     }
-    else if(strcmp(argv[1],"csv") != 0){
-        //check arguments and types
-        printf("invalid command line arguments\n");
-        printf("Use: csv [-frh] [-max field] [-min field] [-mean field] [-records field value] file\n");
+
+    char* frh = argv[2];
+    char* max = "-max";
+    char* min = "-min";
+    char* mean = "-mean";
+    char* fieldName;
+    char* file = argv[argc-1];
+
+    if(arguments == 4 ){
+        if(strcmp(frh,"-f") == 0){
+            commandF(file);
+        }
+        else if(strcmp(frh,"-r") == 0){
+            commandR(file);
+        }
+        else if (strcmp(frh,"h") == 0){
+            commandH(file);
+        }
     }
     else{
-        // parse command line arguments
+        fieldName = argv[4];
+        if(strcmp(frh,"-f") == 0 || strcmp(frh,"-r") == 0 || strcmp(frh,"-h") == 0){
+            // chek if its max, min, mean or record
+            if(max == argv[3]){
+                maxField(frh, fieldName, fieldName);
+            }
+            else if(min == argv[3]){
+                minField(frh, fieldName, fieldName);
+            }
+            else if(mean == argv[3]){
+                meanField(frh, fieldName, fieldName);
+            }
+            else recordsFieldValue(frh, fieldName, fieldName);
+        }
+        else printError();
     }
     return 0;
 }
@@ -32,7 +67,22 @@ int main(int argc, char *argv[]){
 void commandF(char* fileName){}
 void commandR(char* fileName){}
 void commandH(char* fileName){}
-void maxField(char* fileName){}// not sure for parameters
-void minField(char* fileName){}
-void meanField(char* fileName){}
-void recordsFieldValue(char* fileName){}
+void maxField(char* frhCmd, char* field, char* fileName){}
+void minField(char* frhCmd, char* field, char* fileName){}
+void meanField(char* frhCmd, char* field, char* fileName){}
+void recordsFieldValue(char* frhCmd, char* field, char* fileName){}
+
+void printError(){
+    printf("invalid command line arguments\n");
+    printf("Use: csv [-frh] [-max field] [-min field] [-mean field] [-records field value] file\n");
+}
+
+int checkInputError(int numberOfArguments, char* argu){
+    int invalidArgs = -1;
+    if(numberOfArguments == 4 || numberOfArguments == 6 && strcmp(argu,"csv") == 0){
+        return numberOfArguments;
+    }
+    else{
+        return invalidArgs;
+    }
+}
